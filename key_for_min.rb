b37c7dde78921f44a95a9265a556136be03f4b9f
Nina Kazzim <kazzim@icloud.com>
2019-06-08 09:12:51 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,21 @@
 
 def key_for_min_value(name_hash)
 
-end
+    if name_hash == {}
+      return nil
+    else
+      name_a = name_hash.to_a
+
+      d_value= 1000
+      d_key= 0
+      name_a.each do |i|
+         if i[1] < d_value
+           d_value = i[1]
+           d_key = i[0]
+         end
+
+      end
+    end
+   return d_key
+
+ end
