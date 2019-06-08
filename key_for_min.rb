b656338fe15ca8ca8d9a913d9728fba0cad97845
Nina Kazzim <kazzim@icloud.com>
2019-06-08 09:12:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,22 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash.length == 0
-    return nil
-  else
-    name_hash.collect do |name, num|
-      
-end
+
+    if name_hash == {}
+      return nil
+    else
+      name_a = name_hash.to_a
+
+      d_value= 1000
+      d_key= 0
+      name_a.each do |i|
+         if i[1] < d_value
+           d_value = i[1]
+           d_key = i[0]
+         end
+
+      end
+    end
+   return d_key
+
+ end
